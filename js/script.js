1a9e01cf26f9c033eeb6729f361dcc4e49bb870f
Chengbin Du <robertduu@gmail.com>
2024-10-15 23:43:50 +0800

    adding button click event listener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 function init(){
   //add your javascrip between these two lines of code
+
+  const btn = document.getElementById('entrybutton');
+  btn.addEventListener('click', function(event) {
+    const input = document.getElementById('entryinput');
+    const output = document.getElementById('textoutput');
+    output.innerHTML = input.value;
+    alert('Chengbin Du: ' + input.value);
+  })
 }
 window.addEventListener('load', init);
